d4e7a9934997aa8f50f430b00a93a57d80619bf0
ArnieYuan <pkuying@gmail.com>
2024-05-21 07:54:10 -0700

    Fix the ghpages script

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -1,16 +1,16 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-	'docs',
+	'dist',
 	{
 		repo: 'https://github.com/arnieyuan/vanroll.git',
-		message: 'published https://arnieyuan.github.io/vanroll',
+		message: 'published https://arnieyuan.github.io/vanroll/',
 	},
 	function(err) {
 		if (err) {
 			console.error(err);
 		} else {
-			console.log('published https://arnieyuan.github.io/vanroll');
+			console.log('published https://arnieyuan.github.io/vanroll/');
 		}
 	},
 );
